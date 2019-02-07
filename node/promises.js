1afcee36f4cbb1b15a72b85dea54758f5a667486
Lethworgen <Lethworgen@hotmail.com>
2019-02-06 21:05:47 -0800

    Add files via upload

--- node/promises.js
+++ node/promises.js
@@ -5,6 +5,9 @@ const promiseExample = () => {
 };
 
 promiseExample()
-  .then((res) => console.log(res));
+  .then((res)=> res + ' world')
+  .catch((e)=> console.log('I am error :('))
+  .then((res) => console.log(res))
+  .catch((e)=> console.log('I am error :('));
 
 console.log('hi');
